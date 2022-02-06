68c89c829bdfefe4eea599c4fda51df61a177cc4
Alireza Jahandoost <alireza.jhd2000@gmail.com>
2022-02-06 13:36:39 +0330

    create CreateTodo and RemoveTodo action creators

---
+++ src/todos/actions.js
@@ -0,0 +1,11 @@
+export const CREATE_TODO = "CREATE_TODO";
+export const CreateTodo = (text) => ({
+  type: CREATE_TODO,
+  payload: { text },
+});
+
+export const REMOVE_TODO = "REMOVE_TODO";
+export const RemoveTodo = (text) => ({
+  type: REMOVE_TODO,
+  Payload: { text },
+});
