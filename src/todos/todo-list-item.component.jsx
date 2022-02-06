aa4e8f596128a13069176b06667f92ccfd88eb0a
Alireza Jahandoost <alireza.jhd2000@gmail.com>
2022-02-06 16:36:23 +0330

    Implement mark todos as completed feature

--- src/todos/todo-list-item.component.jsx
+++ src/todos/todo-list-item.component.jsx
@@ -1,13 +1,22 @@
 import { connect } from "react-redux";
-import { RemoveTodo } from "./actions";
+import { RemoveTodo, CompleteTodo } from "./actions";
 import "./todo-list-item.styles.css";
 
-const TodoListItem = ({ todo, onRemovePressed }) => {
+const TodoListItem = ({ todo, onRemovePressed, onMarkAsCompletePressed }) => {
   return (
     <div className="todo-item-container">
       <h3>{todo.text}</h3>
       <div className="buttons-container">
-        <button className="completed-button">Mark as Completed</button>
+        {!todo.isCompleted && (
+          <button
+            className="completed-button"
+            onClick={() => {
+              onMarkAsCompletePressed(todo.text);
+            }}
+          >
+            Mark as Completed
+          </button>
+        )}
         <button
           onClick={() => {
             onRemovePressed(todo.text);
@@ -23,6 +32,7 @@ const TodoListItem = ({ todo, onRemovePressed }) => {
 
 const mapDispatchToProps = (dispatch) => ({
   onRemovePressed: (text) => dispatch(RemoveTodo(text)),
+  onMarkAsCompletePressed: (text) => dispatch(CompleteTodo(text)),
 });
 
 export default connect(null, mapDispatchToProps)(TodoListItem);
