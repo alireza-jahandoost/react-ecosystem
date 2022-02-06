3ec8405e95b79d13809bfb500a7c5672866ee959
Alireza Jahandoost <alireza.jhd2000@gmail.com>
2022-02-06 14:57:38 +0330

    Connect todo-list-item component to Redux

--- src/todos/todo-list-item.component.jsx
+++ src/todos/todo-list-item.component.jsx
@@ -1,15 +1,28 @@
+import { connect } from "react-redux";
+import { RemoveTodo } from "./actions";
 import "./todo-list-item.styles.css";
 
-const TodoListItem = ({ todo }) => {
+const TodoListItem = ({ todo, onRemovePressed }) => {
   return (
     <div className="todo-item-container">
-      <h3>{todo.txt}</h3>
+      <h3>{todo.text}</h3>
       <div className="buttons-container">
         <button className="completed-button">Mark as Completed</button>
-        <button className="remove-button">Remove</button>
+        <button
+          onClick={() => {
+            onRemovePressed(todo.text);
+          }}
+          className="remove-button"
+        >
+          Remove
+        </button>
       </div>
     </div>
   );
 };
 
-export default TodoListItem;
+const mapDispatchToProps = (dispatch) => ({
+  onRemovePressed: (text) => dispatch(RemoveTodo(text)),
+});
+
+export default connect(null, mapDispatchToProps)(TodoListItem);
