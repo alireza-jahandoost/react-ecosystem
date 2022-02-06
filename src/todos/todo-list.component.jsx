579e9738f3fa106e03e5050d66a5510c9fc30b20
Alireza Jahandoost <alireza.jhd2000@gmail.com>
2022-02-06 17:59:12 +0330

    Implement getting todos from server

--- src/todos/todo-list.component.jsx
+++ src/todos/todo-list.component.jsx
@@ -1,30 +1,55 @@
+import { useEffect } from "react";
 import { connect } from "react-redux";
 import { RemoveTodo, CompleteTodo } from "./actions";
+import { loadTodos } from "./thunks";
 import TodoListItem from "./todo-list-item.component";
 import NewTodoForm from "./new-todo-form.component";
 import "./todo-list.styles.css";
 
-const TodoList = ({ todos = [], onMarkAsCompletePressed, onRemovePressed }) => (
-  <div className="list-wrapper">
-    <NewTodoForm />
-    {todos.map((todo) => (
-      <TodoListItem
-        onMarkAsCompletePressed={onMarkAsCompletePressed}
-        onRemovePressed={onRemovePressed}
-        key={todo.text}
-        todo={todo}
-      />
-    ))}
-  </div>
-);
+const TodoList = ({
+  todos = [],
+  onMarkAsCompletePressed,
+  onRemovePressed,
+  isLoading,
+  startLoadingTodos,
+}) => {
+  useEffect(() => {
+    startLoadingTodos();
+  }, []);
+
+  const loadingMessage = <div>Loading todos...</div>;
+  const content = (
+    <>
+      {todos.map((todo) => (
+        <TodoListItem
+          onMarkAsCompletePressed={onMarkAsCompletePressed}
+          onRemovePressed={onRemovePressed}
+          key={todo.text}
+          todo={todo}
+        />
+      ))}
+    </>
+  );
+
+  const wrapper = (ui) => (
+    <div className="list-wrapper">
+      <NewTodoForm />
+      {ui}
+    </div>
+  );
+
+  return wrapper(isLoading ? loadingMessage : content);
+};
 
 const mapStateToProps = (state) => ({
   todos: state.todos,
+  isLoading: state.isLoading,
 });
 
 const mapDispatchToProps = (dispatch) => ({
   onRemovePressed: (text) => dispatch(RemoveTodo(text)),
   onMarkAsCompletePressed: (text) => dispatch(CompleteTodo(text)),
+  startLoadingTodos: () => dispatch(loadTodos()),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
